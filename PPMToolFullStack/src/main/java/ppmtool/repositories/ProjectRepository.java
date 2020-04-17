2dd58249274a58e6f5ad3bcf8830403e71c9a188
Emil <pillar_box@bk.ru>
2020-04-18 00:12:33 +0300

    added custom Exception handling

--- PPMToolFullStack/src/main/java/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,12 @@
 package ppmtool.repositories;
 
-public class ProjectRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
--- PPMToolFullStack/src/main/java/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/ppmtool/services/ProjectService.java
@@ -1,4 +1,28 @@
 package ppmtool.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ppmtool.domain.Project;
+import ppmtool.exceptions.ProjectIdException;
+import ppmtool.repositories.ProjectRepository;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exist");
+        }
+
+    }
+
+
+
 }
