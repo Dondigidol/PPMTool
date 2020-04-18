9d17d952ee227bd5c4381398c1bc8f93f924e5c5
Emil <pillar_box@bk.ru>
2020-04-18 09:16:43 +0300

    added custom Exception handling

--- PPMToolFullStack/src/main/java/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/ppmtool/services/ProjectService.java
@@ -23,6 +23,30 @@ public class ProjectService {
 
     }
 
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null){
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' does not exist");
+        }
+        return project;
+    }
+
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null){
+            throw new ProjectIdException("Cannot delete project with ID '" + projectId + "'. This project doesn't exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
 
 
 }
